e12ec21fa6ec2da343a459b8d79b74e8b5a5ff0d
t2024-m0188 <t2024-m0188@t2024-m0188ui-MacBookAir.local>
2025-03-26 11:19:37 +0900

    Feat: stationList 클릭 시 이전 화면으로 이동

--- lib/station_list_page.dart
+++ lib/station_list_page.dart
@@ -11,46 +11,51 @@ class StationListPage extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          station('수서'),
-          station('동탄'),
-          station('평택지제'),
-          station('천안아산'),
-          station('오송'),
-          station('대전'),
-          station('김천구미'),
-          station('동대구'),
-          station('경주'),
-          station('울산'),
-          station('부산'),
+          station('수서', context),
+          station('동탄', context),
+          station('평택지제', context),
+          station('천안아산', context),
+          station('오송', context),
+          station('대전', context),
+          station('김천구미', context),
+          station('동대구', context),
+          station('경주', context),
+          station('울산', context),
+          station('부산', context),
         ],
       ),
     );
   }
 
-  Widget station(String station){
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          alignment: Alignment.centerLeft,
-          height: 50,
-          decoration: BoxDecoration(
-            border: Border(bottom: BorderSide(
-              color: Colors.grey.shade300,
-            )),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(left: 20,),
-            child: Text(
-              station,
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+  Widget station(String station, BuildContext context){
+    return GestureDetector(
+      onTap: (){
+        Navigator.pop(context);
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            height: 50,
+            decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(
+                color: Colors.grey.shade300,
+              )),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20,),
+              child: Text(
+                station,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
